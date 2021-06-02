d2dac30212e5ec890ecc9b1f159483d612cab5c2
Lac Akkarapon <lacakkarapon@gmail.com>
2021-06-02 14:11:41 +0700

    EP 7.7
    - เรียนรู้การเขียนแบบ Single File Components (SFC)
    
    - http-vue-loader

---
+++ components/blog.vue
@@ -0,0 +1,19 @@
+<template>
+    <p class="hello">Hello {{who}}</p>
+</template>
+ 
+<script>
+module.exports = {
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+ 
+<style scoped>
+p {
+    background-color: #ffe;
+}
+</style>
